54a7cfd16b4a45a8b2963d3e87dcd7049751f968
Md Eftequarul Alam <efthaqur@gmail.com>
2025-09-19 02:03:39 +0600

    custom css added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = ({countriesPromise}) => {
     
@@ -11,12 +12,15 @@ const Countries = ({countriesPromise}) => {
     
     return (
         <div>
-            <h1>In the countries: {countries.length}</h1>
-            {
+            <h1 className='text-center'>In the countries: {countries.length}</h1>
+            <div className='countries'>
+                {
                 countries.map( country => <Country 
                     key ={country.cca3.cca3}
                     country = {country}></Country>  )
             }
+            </div>
+                
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
-    console.log(country.currencies.currencies);
+    console.log(country.languages.languages);
     
     return (
-        <div className=''>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common} </h3>
             <p>Official Name: {country.name.official}</p>
             <p>Region: {country.region.region}</p>
             <p>Capital: {country.capital.capital}</p>
             <p>Population: {country.population.population}</p>
+            <p>Population: {country.languages}</p>
             <p>Area: {country.area.area}</p>
             <p>Coninents: {country.continents.continents}</p>
 
